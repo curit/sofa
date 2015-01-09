6eb5881912715c561f50f1dedff9b1dfbe16f3b6
Albert-Jan Nijburg <albertjan@curit.com>
2015-01-09 14:21:05 +0100

    initial commit

---
+++ src/sofa/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("sofa")>]
+[<assembly: AssemblyProductAttribute("sofa")>]
+[<assembly: AssemblyDescriptionAttribute("CouchDB client in f#")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/sofa.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module sofa.Tests
+
+open sofa
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
