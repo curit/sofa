d3afd7d9572de84bfe784687fd2fd523ad77f451
Albert-Jan Nijburg <albertjan@curit.com>
2015-01-16 14:06:11 +0100

    start implementing get and head

--- src/sofa/Library.fs
+++ src/sofa/Library.fs
@@ -1,24 +1,57 @@
 namespace sofa
 
+open System
+
+open FSharp.Data
+
+[<AutoOpen>]
+module Convenience = 
+    let mapHeaders headers = 
+        headers |> Map.map (fun k (v:string) -> 
+                        v.Split([|","|], StringSplitOptions.RemoveEmptyEntries) 
+                        |> Seq.map (fun s -> s.Trim ()) 
+                        |> Seq.filter (not << String.IsNullOrWhiteSpace)
+                        |> Seq.toList
+                    )
+
 type Database = 
     {
         Url: string
     }
     member x.NormalizeUrl () =
-        { x with 
-            Url = 
-                match x.Url.[x.Url.Length - 1] with 
-                | '/' -> x.Url
-                | _ -> x.Url + "/"
-        }
+        match x.Url.[x.Url.Length - 1] with 
+        | '/' -> x.Url
+        | _ -> x.Url + "/"
+
+type SeatedSofa<'id, 'obj> = 
+    {
+        get: ('id -> 'obj Async)
+        head: ('id -> Map<string, string list> Async)
+    } 
 
 module Sofa =
+    /// get a resource from the db
+    let get (db:Database) ser http id = 
+        async {
+            let! res = http (sprintf "%s%O" (db.NormalizeUrl ()) id) 
+            return res |> ser
+        }
+    
+    let head (db:Database) http id = 
+        async {
+            return! http (sprintf "%s%O" (db.NormalizeUrl ()) id)
+        }
+                 
+    let build db ser = 
+        let headReq url =
+            async { 
+                let! res = Http.AsyncRequest (url, httpMethod = "HEAD")
+                return res.Headers |> mapHeaders
+            }
 
-    /// Returns 42
-    ///
-    /// ## Parameters
-    ///  - `num` - whatever
-    let get id db ser http =
-        http (sprintf "%s%O" db.Url id) |> ser
-             
+        {
+            get = get db ser Http.AsyncRequestString
+            head = head db headReq
+        }
+
     
--- tests/sofa.Tests/Tests.fs
+++ tests/sofa.Tests/Tests.fs
@@ -1,10 +1,88 @@
 module sofa.Tests
 
 open sofa
-open xunit
+open Xunit
+open FsUnit.Xunit
+
+type HttpResult = {
+    Url: string
+    Result: string
+}
+
+let testHttpGet ret = 
+    let returnResult str = 
+        async {
+            return { 
+                Url = str
+                Result = ret
+            }
+        }
+
+    returnResult
+
+let testHttpHead ret = 
+    let returnResult str = 
+        async {
+            return 
+                [
+                    ("_rev", ["1-" + ret])
+                ] |> Map.ofList
+        }
+
+    returnResult
 
 [<Fact>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``basic head test`` () =
+    async {
+        // Given
+        let db:Database = { Url = "test://bla" }
+        let http = testHttpHead "blaat"
+
+        // When
+        let! result = Sofa.head db http 5 
+
+        // Then
+        (result |> Map.toList).[0] |> should equal ("_rev", ["1-blaat"])
+    } |> Async.RunSynchronously
+
+[<Fact>]
+let ``basic get test`` () =
+    async {
+        // Given
+        let db:Database = { Url = "test://bla" }
+        let http = testHttpGet "blaat"
+
+        // When
+        let! result = Sofa.get db (fun a -> a) http 5
+
+        // Then
+        result.Result |> should equal "blaat"
+        result.Url |> should equal "test://bla/5"
+    } |> Async.RunSynchronously
+
+[<Fact>]
+let ``map headers`` () =
+    // Given
+    let headers = 
+        [
+            ("test1", "test")
+            ("test2", "test,test")
+            ("test3", "test, test")
+            ("test4", "test,")
+            ("test5", "test,,test")
+            ("test6", "test, test, test, ")
+            ("test7", "test, , , ")
+        ] |> Map.ofList
+
+    // When
+    let mappedHeaders = headers |> mapHeaders |> Map.toList
+
+    // Then
+    mappedHeaders.[0] |> should equal ("test1", ["test"])
+    mappedHeaders.[1] |> should equal ("test2", ["test"; "test"])
+    mappedHeaders.[2] |> should equal ("test3", ["test"; "test"])
+    mappedHeaders.[3] |> should equal ("test4", ["test"])
+    mappedHeaders.[4] |> should equal ("test5", ["test"; "test"])
+    mappedHeaders.[5] |> should equal ("test6", ["test"; "test"; "test"])
+    mappedHeaders.[6] |> should equal ("test7", ["test"])
+   
