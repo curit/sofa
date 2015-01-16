2ce97cb834266d5e2db449532562e25ded81c956
Albert-Jan Nijburg <albertjan@curit.com>
2015-01-16 11:18:36 +0100

    fix xunit ref

--- src/sofa/Library.fs
+++ src/sofa/Library.fs
@@ -1,16 +1,24 @@
 namespace sofa
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
-module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+type Database = 
+    {
+        Url: string
+    }
+    member x.NormalizeUrl () =
+        { x with 
+            Url = 
+                match x.Url.[x.Url.Length - 1] with 
+                | '/' -> x.Url
+                | _ -> x.Url + "/"
+        }
+
+module Sofa =
+
+    /// Returns 42
+    ///
+    /// ## Parameters
+    ///  - `num` - whatever
+    let get id db ser http =
+        http (sprintf "%s%O" db.Url id) |> ser
+             
+    
